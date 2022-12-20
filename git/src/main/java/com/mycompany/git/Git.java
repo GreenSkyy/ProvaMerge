a1216e954def587a1532de7fb1367837fd3a0dce
Josep Lluis <Josep Lluis@DESKTOP-FJ397B5>
2022-12-20 15:57:00 +0100

    He añadido una nueva linea de código

--- git/src/main/java/com/mycompany/git/Git.java
+++ git/src/main/java/com/mycompany/git/Git.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.git;
 
-/**
- *
- * @author Josep Lluis
- */
 public class Git {
 
     public static void main(String[] args) {
+        int hola = 8;
         System.out.println("Hello World!");
         System.out.println("Buenos dias");
+        System.out.println(hola);
     }
 }
